89f5f494a8bde6b7337326a3cb6e01b6dac70151
kscott244 <kscott244@gmail.com>
2026-03-20 18:38:36 -0400

    Update page.tsx

--- src/app/accelerate/page.tsx
+++ src/app/accelerate/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import AccelerateApp from "@/components/AccelerateApp";
+import { fixGroupNames } from "@/data/name-fix";
+import { PRELOADED } from "@/data/preloaded-data";
+
+if (PRELOADED?.groups) {
+  PRELOADED.groups = fixGroupNames(PRELOADED.groups);
+}
 
 export default function AcceleratePage() {
   return <AccelerateApp />;
